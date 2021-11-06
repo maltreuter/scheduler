52a5439512c859ae5e1bb4c4d950e4df623cbb65
Michael Altreuter <maltreuter@michaels-air.lan>
2021-11-06 12:53:01 -0500

    rudimentary menu set up

--- scheduler.cpp
+++ scheduler.cpp
@@ -4,4 +4,27 @@ using namespace std;
 
 int main(int argc, char **argv) {
 	cout << "Guh Daddy" << endl;
+
+	int scheduler;
+	int n_queues = 0;
+	string input_file;
+
+	cout << "Select an input file: ";
+	cin >> input_file;
+
+	cout << "Select a scheduler: " << endl;
+	cout << "1. Multi-level Feedback Queue Scheduler" << endl;
+	cout << "2. Real-Time Scheduler" << endl;
+	cin >> scheduler;
+
+	if(scheduler == 1) {
+		cout << "How many queues?" << endl;
+		cin >> n_queues;
+
+		cout << "Running MFQS with " << n_queues << " queues and input file: " << input_file << endl;
+	} else {
+		cout << "Running RTS with input file: " << input_file << endl;
+	}
+
+
 }
